fce4b98b7538dc0c782d75985de6b5db645f28d1
ayanamist <ayanamist@gmail.com>
2013-05-31 14:29:14 +0800

    Init structure.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a RSS builder for Sina Weibo running on Google App Engine.
---
+++ application/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import os
+import sys
+
+import webapp2
+
+lib_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+for dir_path in os.listdir(lib_path):
+    sys.path.insert(0, os.path.join(lib_path, dir_path))
+
+from application.utils import monkey_patch
+
+monkey_patch.patch_all()
+
+config = dict((name, os.environ[name]) for name in (
+    "SITE_NAME",
+    "SESSION_COOKIE_PATH",
+    "CONSUMER_KEY",
+    "CONSUMER_SECRET",
+    "SECRET_KEY",
+))
+
+app = webapp2.WSGIApplication([
+    ("/", ""),
+], config=config, debug=True)
+
